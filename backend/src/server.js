2243d9554f7e676b22a62b5ea0349edd204c4169
alexandreerick <erickalexandrex@gmail.com>
2019-10-01 13:18:59 -0300

    Primeiro Commit

---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+// Importando Dependencias e outras coisas
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+// Criando o App
+const app = express();
+
+// Conectando com a DB
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@omnistack-4hmro.mongodb.net/semana09?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+// Require e Response
+
+// req.query = acessar query params (para filtros)
+// req.params = Acessar route params (para ediçao e delete)
+// req.body = Acessar corpo da requisiçao (Para criaçao e ediçao)
+
+// Configurar o Express para usar json
+app.use(express.json());
+
+app.use(routes);
+
+// Escutar a porta
+app.listen(3333);
