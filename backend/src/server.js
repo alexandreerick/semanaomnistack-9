76a5b29cb1107e6773602ce1bba0e2d1e25b18db
alexandreerick <erickalexandrex@gmail.com>
2019-10-03 14:52:22 -0300

    Finalizacao do frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const routes = require("./routes");
 
@@ -28,7 +29,7 @@ app.use(cors());
 
 // Configurar o Express para usar json
 app.use(express.json());
-
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 // Escutar a porta
